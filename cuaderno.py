83fe6db5d28e903625d89e59822a10eed9e11bc2
jona69846 <j.solorzano@uinenlinea.mx>
2022-06-22 13:18:51 -0500

    first commit

---
+++ cuaderno.py
@@ -0,0 +1,16 @@
+print("dame el numero A") 
+numA= int (input())
+while n<=numA:
+    n=numA
+print(n)
+
+n=n+1
+
+print("dame el numero B")
+numB=int (input())
+
+print("el valor del numero A", numA)
+print("el valor del numero B", numB)
+
+suma=numA+numB
+print("la suma es",suma)
