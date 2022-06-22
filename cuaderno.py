52f55123f9f71dd5d4b2976391d36f834dfcfdc2
jona69846 <j.solorzano@uinenlinea.mx>
2022-06-22 13:51:15 -0500

    first commit

--- cuaderno.py
+++ cuaderno.py
@@ -1,6 +1,6 @@
 print("dame el numero A") 
 numA= int (input())
-while n<=numA:
+while n>=numB:
     n=numA
 print(n)
 
@@ -8,9 +8,13 @@ n=n+1
 
 print("dame el numero B")
 numB=int (input())
-
+while n>=numA:
+    n=numB
+print(n)
+n=n*1
 print("el valor del numero A", numA)
 print("el valor del numero B", numB)
 
 suma=numA+numB
 print("la suma es",suma)
+
